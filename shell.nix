eed452371b04019f03a439979068c9eb33fbad7d
woxQAQ <woxqaq@gmail.com>
2025-09-09 02:29:39 +0800

    chore: tidy shell.nix

--- shell.nix
+++ shell.nix
@@ -9,28 +9,10 @@ pkgs.mkShell {
     # Go is required for leetgo
     leetgo
     python3
-    (writeShellScriptBin "ltest" ''
-      ${pkgs.leetgo}/bin/leetgo test last
-    '')
-    (writeShellScriptBin "lpush" ''
-      ${pkgs.leetgo}/bin/leetgo submit last
-    '')
-    (writeShellScriptBin "lpick" ''
-      if [ -z "$1" ]; then
-        echo "❌ Please provide a question ID"
-        echo "Usage: lpick <qid>"
-        return 1
-      fi
-      ${pkgs.leetgo}/bin/leetgo pick "$1"
-    '')
   ];
 
   shellHook = ''
     echo "🚀 LeetCode development environment activated"
-    echo "📝 Available commands:"
-    echo "   lpick <qid>    - Pick a LeetCode problem (leetgo pick <qid>)"
-    echo "   ltest          - Test the last problem (leetgo test last)"
-    echo "   lpush          - Submit the last problem (leetgo submit last)"
     echo ""
 
     exec ${pkgs.zsh}/bin/zsh
